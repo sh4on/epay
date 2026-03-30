bb7ceba6442068513d673c13a642f516b4390023
shaon <shaonx@proton.me>
2026-03-30 22:21:53 +0600

    add section header widget

---
+++ lib/shared/common_widgets/section_header.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../../core/constants/app_spacing.dart';
+import '../../core/constants/app_typography.dart';
+
+// section label used in home and contact lists
+class SectionHeader extends StatelessWidget {
+  final String title;
+  final EdgeInsets? padding;
+
+  const SectionHeader({
+    super.key,
+    required this.title,
+    this.padding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: padding ??
+          const EdgeInsets.symmetric(vertical: AppSpacing.md),
+      child: Text(
+        title,
+        style: AppTypography.headlineSmall,
+      ),
+    );
+  }
+}
